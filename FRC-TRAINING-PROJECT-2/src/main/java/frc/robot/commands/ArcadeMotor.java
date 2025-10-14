ec5a1dd63e08ab1c51f5c22bf81a37555661b4a0
quinnyen88-lgtm <quinnyen88@gmail.com>
2025-10-13 23:40:52 -0700

    Saving motor setup

---
+++ FRC-TRAINING-PROJECT-2/src/main/java/frc/robot/commands/ArcadeMotor.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.IOConstants;
+import frc.robot.constants.MotorConstants;
+import frc.robot.subsystems.Motor;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class ArcadeMotor extends Command {
+
+  private Motor m_motor;
+  private Joystick m_joystick;
+  private double m_speed;
+
+  /** Creates a new ArcadeMotor. */
+  public ArcadeMotor(Motor motor, Joystick joystick) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_motor = motor; 
+    m_joystick = joystick;
+    addRequirements(m_motor);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double joystickAxis = m_joystick.getRawAxis(IOConstants.kMotorControlAxis);
+    m_speed = joystickAxis * MotorConstants.kSpeedMultiplier;
+    m_motor.setSpeed(m_speed);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ FRC-TRAINING-PROJECT-2/src/main/java/frc/robot/subsystems/Motor.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.security.Key;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.MotorConstants;
+
+public class Motor extends SubsystemBase {
+
+  SparkMax m_motor = new SparkMax(MotorConstants.kMotorID, MotorType.kBrushless);
+  SparkMaxConfig m_config = new SparkMaxConfig();
+
+
+  /** Creates a new Motor. */
+  public Motor() {
+    m_config.idleMode(IdleMode.kBrake);
+    m_motor.configure(m_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber( "Motor Speed", getSpeed());
+  }
+
+  public void setSpeed(double speed) {
+    m_motor.set(speed);
+  }
+
+  public double getSpeed() {
+    return m_motor.get();
+  }
+}
