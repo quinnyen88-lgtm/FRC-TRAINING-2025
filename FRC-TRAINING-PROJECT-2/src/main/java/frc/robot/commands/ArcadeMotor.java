db3af3282654c770ae2511941b733e35fc59a7ed
quinnyen88-lgtm <quinnyen88@gmail.com>
2025-10-14 18:11:22 -0700

    Need Testing for Arcade Motor.java

--- FRC-TRAINING-PROJECT-2/src/main/java/frc/robot/commands/ArcadeMotor.java
+++ FRC-TRAINING-PROJECT-2/src/main/java/frc/robot/commands/ArcadeMotor.java
@@ -39,7 +39,9 @@ public class ArcadeMotor extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_motor.setSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
